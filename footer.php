f29bc546606cf24aa4c26fb6ae97e0344d4bc5cd
SM Maruph <smmaruph.bhbd2001@gmail.com>
2024-03-27 00:05:27 +0600

    footer added

---
+++ footer.php
@@ -0,0 +1,47 @@
+
+<section class="footer">
+      <div class="footer-row">
+        <div class="footer-col">
+          <h4>Info</h4>
+          <ul class="links">
+            <li><a href="http://localhost/project/aboutus.php">About Us</a></li>
+            <li><a href="#">Terms & Condition</a></li>
+            <li><a href="#">Privacy policy</a></li>
+            <li><a href="#">Contact us</a></li>
+            <li><a href="#">developer contact</a></li>
+          </ul>
+        </div>
+        <div class="footer-col">
+          <h4>Explore</h4>
+          <ul class="links">
+            <li><a href="http://localhost/project/aboutus.php">Home</a></li>
+            <li><a href="#">Top Restaurants</a></li>
+            <li><a href="#">Popular Now</a></li>
+            <li><a href="#">Hot deals</a></li>
+            <li><a href="#">Discount</a></li>
+          </ul>
+        </div>
+        <div class="footer-col">
+          <h4>Legal</h4>
+          <ul class="links">
+            <li><a href="#">Customer Agreement</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Security</a></li>
+          </ul>
+        </div>
+        <div class="footer-col">
+          <div class="icons">
+            <i class="fa-brands fa-facebook-f"></i>
+            <i class="fa-brands fa-twitter"></i>
+            <i class="fa-brands fa-linkedin"></i>
+            <i class="fa-brands fa-github"></i>
+          </div>
+          <p>
+            @Copyright Bite Map 2023.All rights reserved.
+          </p>
+          <p>Developed by S.M.Shahriar Rahman Maruph</p>
+        </div>
+      </div>
+    </section>
+
+</body>
